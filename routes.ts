49e35ff040a2fa26c0677d309841a15b90b3c1aa
Tom Mount <tmountjr@gmail.com>
2022-10-24 16:00:37 -0400

    better documentation of what's going on

--- routes.ts
+++ routes.ts
@@ -1,5 +1,4 @@
-// This file was added by layer0 init.
-// You should commit this file to source control.
+/* eslint-disable no-console */
 
 import { Router } from '@layer0/core/router'
 import { nuxtRoutes } from '@layer0/nuxt'
@@ -7,15 +6,30 @@ import { nuxtRoutes } from '@layer0/nuxt'
 export default new Router()
   // Prevent search engines from indexing permalink URLs
   .noIndexPermalink()
-  .match('/api/:slug*', ({ request, renderWithApp }) => {
+
+  // Ensure that the server middleware routes are accounted for on the L0 side.
+  .match('/api/:slug*', ({ request, cache, renderWithApp }) => {
+    // Log the requests and their request bodies.
     console.log(
       `[${Date.now()}] ${request.method} ${
         request.url
       } ${request.rawBody?.toString()}`
     )
+
+    // Do not cache.
+    cache({
+      browser: { maxAgeSeconds: 0 },
+      edge: { maxAgeSeconds: 0 }
+    })
+
+    // Send to the app.
     renderWithApp()
   })
+
+  // Attach the service worker.
   .match('/service-worker.js', ({ serviceWorker }) => {
     serviceWorker('.nuxt/dist/client/service-worker.js')
   })
+
+  // Parse additional nuxt routes.
   .use(nuxtRoutes)
