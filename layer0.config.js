ac995b3c35c7bbeaab1c30710b939ea69ac674ec
Tom Mount <tmountjr@gmail.com>
2022-11-14 16:44:13 -0500

    remove api backend and l0 intercept

--- layer0.config.js
+++ layer0.config.js
@@ -4,12 +4,6 @@
 // Learn more about this file at https://docs.layer0.co/guides/layer0_config
 module.exports = {
   connector: '@layer0/nuxt',
-  includeNodeModules: true,
+  includeNodeModules: true
   // includeFiles: require('./getNodeModules.js')
-  backends: {
-    api: {
-      domainOrIp: 'demos-fsxa-api-server-default.layer0-limelight.link',
-      hostHeader: 'demos-fsxa-api-server-default.layer0-limelight.link'
-    }
-  }
 }
--- routes.ts
+++ routes.ts
@@ -7,13 +7,6 @@ export default new Router()
   // Prevent search engines from indexing permalink URLs
   .noIndexPermalink()
 
-  // Intercept any routes that would trigger the infinite loop of the FSXAProxyApi
-  // and dispatch them to a dedicated instance of the FSXARemoteApi.
-  // Configure the deployment details for the API backend in layer0.config.js.
-  .match('/api/:slug*', ({ proxy }) => {
-    proxy('api')
-  })
-
   // Attach the service worker.
   .match('/service-worker.js', ({ serviceWorker }) => {
     serviceWorker('.nuxt/dist/client/service-worker.js')
